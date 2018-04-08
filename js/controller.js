7ca4ac487d041da4335645edb5ca479d687f546a
Chul Ahn <scahn.x@gmail.com>
2018-04-08 04:10:16 -0400

    getting started
    page where clicking button, returns data.
    
    linked controller to work with ejs.
    
    test translation calls

---
+++ js/controller.js
@@ -0,0 +1,57 @@
+(function(angular) {
+    
+        angular.module('app',[])
+
+            
+    
+            .controller("dataController", [ '$scope', '$http' , function ($scope, $http) {
+
+
+                $scope.clicked = function() { 
+
+                    var reqTest = {
+                        method: 'GET',
+                        url: '/test'
+                    };
+
+                    $http(reqTest).then(function successCallback(response) {
+                        console.log("success");
+                        console.log(response);
+                    }, function errorCallback(response) {
+                        console.log("fail");
+                    });
+
+                    // var req = {
+
+                    //     method: 'POST',
+                    //      url: 'https://translation.googleapis.com/language/translate/v2',
+                    //      headers: {
+                    //         'Authorization' : 'Bearer ya29.c.El-XBSsYOR6NF8mFchcfv2vOBYllYUUD89nu02Ms0U6hbiSAId9qjqTOyS0L5kQdAwmIJQDKnXPwyuksvxXe04hSVPR3Bny6rE3qDKbSw8wd1P7pqvzIF8QL0OxxB5UZCA',
+                    //        'Content-Type': 'application/json'
+                    //      },
+                    //      data: {
+                    //         'q': 'The Great Pyramid of Giza (also known as the Pyramid of Khufu or the Pyramid of Cheops) is the oldest and largest of the three pyramids in the Giza pyramid complex.',
+                    //         'source': 'en',
+                    //         'target': 'es',
+                    //         'format': 'text'
+                    //     }
+                    // }
+
+                    // $http(req).then(function successCallback(response) {
+                    //     console.log("success");
+                    //     console.log(response);
+                    // }, function errorCallback(response) {
+                    //     console.log("fail");
+                    // })
+
+                    // var xhr = new XMLHttpRequest();
+                    // xhr.open('POST', 'https://translation.googleapis.com/language/translate/v2');
+                    // xhr.setRequestHeader('Authorization' , 'Bearer ya29.c.El-XBSsYOR6NF8mFchcfv2vOBYllYUUD89nu02Ms0U6hbiSAId9qjqTOyS0L5kQdAwmIJQDKnXPwyuksvxXe04hSVPR3Bny6rE3qDKbSw8wd1P7pqvzIF8QL0OxxB5UZCA');
+                    // xhr.setRequestHeader('Content-Type', 'application/json');
+                    // xhr.send();
+
+                };
+                
+            }]);
+    })(window.angular);
+    
---
+++ js/index.js
@@ -0,0 +1,105 @@
+var http = require("http");
+var express = require("express");
+var app = express();
+var path = require('path');
+
+
+// Imports the Google Cloud client library
+const Translate = require('@google-cloud/translate');
+
+// Your Google Cloud Platform project ID
+const projectId = 'data-visitor-127620';
+
+// Instantiates a client
+const translate = new Translate({
+  projectId: projectId,
+});
+
+// The text to translate
+const text = 'Hello, world!';
+// The target language
+const target = 'ru';
+
+
+
+// // Imports the Google Cloud client library
+// const Translate = require('@google-cloud/translate');
+
+// // Creates a client
+// const translate = new Translate();
+
+// /**
+//  * TODO(developer): Uncomment the following lines before running the sample.
+//  */
+// // const text = 'The text to translate, e.g. Hello, world!';
+// // const target = 'The target language, e.g. ru';
+
+// // Translates the text into the target language. "text" can be a string for
+// // translating a single piece of text, or an array of strings for translating
+// // multiple texts.
+// translate
+//   .translate(text, target)
+//   .then(results => {
+//     let translations = results[0];
+//     translations = Array.isArray(translations)
+//       ? translations
+//       : [translations];
+
+//     console.log('Translations:');
+//     translations.forEach((translation, i) => {
+//       console.log(`${text[i]} => (${target}) ${translation}`);
+//     });
+//   })
+//   .catch(err => {
+//     console.error('ERROR:', err);
+//   });
+
+app.get('/', function (req, res) {
+
+	// res.send('Hello World!');
+	res.sendfile(path.resolve('../index.html'));
+});
+
+
+app.get('/js/controller.js', function(req,res) {
+
+	res.sendfile(path.resolve('controller.js'));
+})
+
+
+app.get('/test', function(req,res) {
+
+	res.send({"Yo": "Hi"});
+})
+
+
+app.listen(8000, function() {
+	console.log("Listening on port 8000");
+
+	// Translates some text into Russian
+	translate
+	  .translate(text, target)
+	  .then(results => {
+	    const translation = results[0];
+
+	    console.log(translation);
+
+	    console.log(`Text: ${text}`);
+	    console.log(`Translation: ${translation}`);
+	  })
+	  .catch(err => {
+	    console.error('ERROR:', err);
+	  });
+});
+
+//Create HTTP server and listen on port 8000 for requests
+// http.createServer(function (request, response) {
+
+//    // Set the response HTTP header with HTTP status and Content type
+//    response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+//    // Send the response body "Hello World"
+//    response.end('Hello World\n');
+// }).listen(8000);
+
+console.log("Server running");
